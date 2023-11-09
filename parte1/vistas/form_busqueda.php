20d56fa79a9bb42e15a5e98a56e371a5249e35ee
Nicolasmateoprietojimenez <nicompj@gmail.com>
2023-11-08 19:21:31 -0500

    Desarrollo web parte 1

---
+++ parte1/vistas/form_busqueda.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Púta</title>
+    <link rel="stylesheet" href="../styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+</head>
+<body>
+    <div class="main-content">
+        <form class="form-search" method="get" action="result_busqueda.php">
+            <input type="search" name="search" placeholder="Buscar...">
+            <button type="submit"><i class="fas fa-search"></i></button>
+        </form>
+    </div>
+</body>
+
+</html>
